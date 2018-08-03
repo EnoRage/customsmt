d87bfd0675aef251bfac7c4d81a8eb9a55087ff0
Nikita Kozlov <nk@bankexfoundation.org>
2018-08-03 16:52:16 +0300

    add a lot of changes

--- main.go
+++ main.go
@@ -1,62 +1,73 @@
 package main
 
 import (
+	"./customsmt"
 	"log"
-	"./merkletree"
-	"github.com/miguelmota/go-solidity-sha3"
 )
 
-//TestContent implements the Content interface provided by merkletree and represents the content stored in the tree.
-type TestContent struct {
-	x string
-}
-
-//CalculateHashBytes hashes the values of a TestContent
-func (t TestContent) CalculateHashBytes() ([]byte, error) {
-	hash := solsha3.SoliditySHA3(
-		solsha3.Bytes32([]byte(t.x)),
-	)
-	return hash, nil
-}
-
-//Equals tests for equality of two Contents
-func (t TestContent) Equals(other merkletree.Content) (bool, error) {
-	return t.x == other.(TestContent).x, nil
-}
 
 func main() {
 	//Build list of Content to build tree
-	var list []merkletree.Content
-	list = append(list, TestContent{x: "Hello"})
-	list = append(list, TestContent{x: "Hi"})
-	list = append(list, TestContent{x: "Hey"})
-	list = append(list, TestContent{x: "Hola"})
+	var content []string
+	content = append(content, "a")
+	content = append(content, "b")
+	//content = append(content, "c")
+	//content = append(content, "d")
 
-	//Create a new Merkle Tree from the list of Content
-	t, err := merkletree.NewTree(list)
-	if err != nil {
-		log.Fatal(err)
-	}
+	sContent := customsmt.CreateContent(content)
 
-	//Get the Merkle Root of the tree
-	mr := t.MerkleRoot()
-	log.Println(mr)
 
-	//Verify the entire tree (hashes for each node) is valid
-	vt, err := t.VerifyTree()
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("Verify Tree: ", vt)
+	t := customsmt.CreateTree(sContent)
 
-	//Verify a specific content in in the tree
-	vc, err := t.VerifyContent(list[0])
-	if err != nil {
-		log.Fatal(err)
-	}
+	log.Println(customsmt.Hashes(t))
 
-	log.Println("Verify Content: ", vc)
+	var content2 []string
+	content2 = append(content2, "hv1ly")
+	content2 = append(content2, "cl444ivv7l")
+	content2 = append(content2, "cl4iv6vl")
+	content2 = append(content2, "cl1iv4v4l")
+	content2 = append(content2, "cl2ivv5l")
+	content2 = append(content2, "4444")
 
-	//String representation
-	log.Println(t)
+	sContent2 := customsmt.CreateContent(content)
+
+	customsmt.Test(sContent2, t)
+
+	log.Println(customsmt.Hashes(t))
+	//log.Println(customsmt.GetMerkleRoot(t))
+	//log.Println(customsmt.VerifySpecificLeaf(t, sContent[1]))
+
+
+	//////Create a new Merkle Tree from the list of Content
+	//t, err := merkletree.NewTree(list)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//////Get the Merkle Root of the tree
+	//mr := t.MerkleRoot()
+	//log.Println(mr)
+	//
+	//////Verify the entire tree (hashes for each node) is valid
+	//vt, err := t.VerifyTree()
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//log.Println("Verify Tree: ", vt)
+	//
+	////Verify a specific content in in the tree
+	//vc, err := t.VerifyContent(list[0])
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	////
+	//log.Println("Verify Content: ", vc)
+	//
+	////String representation
+	//log.Println(t)
+	//
+	//st := t.String()
+	//
+	//log.Println(st)
 }
+
